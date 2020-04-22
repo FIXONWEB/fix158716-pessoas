dc8ff31bf7a6466a28186cce014a50bb65f6967c
fixonweb <contato@fixonweb.com.br>
2020-04-21 21:35:27 -0300

    0.1.26

--- fix158716-pessoas.php
+++ fix158716-pessoas.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/fixonweb
  * Text Domain:     fix158716-pessoas
  * Domain Path:     /languages
- * Version:         0.1.25
+ * Version:         0.1.26
  *
  * @package         Fix158716_Pessoas
  */
